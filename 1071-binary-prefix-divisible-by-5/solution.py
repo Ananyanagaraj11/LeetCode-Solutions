265394dd9ccfc824f85840d0f50de1ac9cebe719
Ananya Naga Raj <ananyanraj6@gmail.com>
2026-04-23 00:43:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (26.49%)

---
+++ 1071-binary-prefix-divisible-by-5/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def prefixesDivBy5(self, nums):
+        result = []
+        remainder = 0
+        for bit in nums:
+            remainder = (remainder * 2 + bit) % 5
+            result.append(remainder == 0)
+        return result
